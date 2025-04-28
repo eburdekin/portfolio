54f1ea008bec338b8f46b9588f6a0431f411022e
Eileen Burdekin <eileenburdekin@proton.me>
2025-04-27 20:40:50 -0700

    add TDM calculator to projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -16,6 +16,17 @@ const Work = () => {
           Free digital tool designed to aid Californians with arrest and
           conviction histories in clearing their criminal records.
         </ProjectCard>
+        <ProjectCard
+          image="../images/tdmcalculator.webp"
+          projectName="TDM Calculator"
+          link1="https://tdm.ladot.lacity.org/about"
+          link2="https://github.com/hackforla/tdm-calculator"
+          technologies={["React", "Node", "Express", "Jest"]}
+        >
+          Web app created with the L.A. Department of Transportation (LADOT) and
+          L.A. City Planning to help real estate developers design better
+          projects for the city.
+        </ProjectCard>
       </div>
     </section>
   );
